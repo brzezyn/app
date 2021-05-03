67e2cfbab600fdc87c72a595be49a4439ad15a5d
Artur Brzeziński <arturitu.xdd@gmail.com>
2021-05-03 20:53:30 +0200

    22.4 Tests

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -16,19 +16,9 @@ public class TrelloController {
 
     private final TrelloClient trelloClient;
 
-    @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList -> {
-                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
-            });
-        });
+    @GetMapping("/boards")
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @PostMapping("createTrelloCard")
@@ -37,13 +27,3 @@ public class TrelloController {
     }
 
 }
-
-//        trelloBoards.stream()
-//                .filter(trelloBoardDto -> trelloBoardDto.getId() != null)
-//                .filter(trelloBoardDto -> trelloBoardDto.getName() != null)
-//                .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Kodilla"))
-//                .forEach(trelloBoardDto -> {
-//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-//        });
-//    }
-//}
